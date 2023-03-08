9fdb91d7df31e3697474d4446460679874adac0b
shreyash <shreyasgaikwad52@gmail.com>
2023-03-08 21:49:48 +0530

    Add

---
+++ src/test/java/java8Features/Draw.java
@@ -0,0 +1,26 @@
+package java8Features;
+interface Draw{
+
+public abstract void  draw ();
+    public class TestDrawNoLambada {
+
+        public static void main(String[] args) {
+
+            int width = 20;
+
+            Draw d = new Draw() {
+
+                @Override
+                public void draw() {
+                    System.out.println("Width is -"+width);
+
+                }
+            };
+
+            d.draw();
+
+        }
+
+    }
+
+}
---
+++ src/test/java/java8Features/Drawable1.java
@@ -0,0 +1,25 @@
+package java8Features;
+
+@FunctionalInterface
+interface Drawable1 {
+
+    public abstract int draw();
+
+    public class TestDrawLambadaReturn {
+
+        public static void main(String[] args) {
+
+            int width = 20;
+
+            Drawable1 d = () -> {
+
+                System.out.println("in draw method");
+                return width;
+            };
+
+            System.out.println(d.draw());
+
+        }
+
+    }
+}
